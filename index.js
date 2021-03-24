e4d4f96c9017ace888b1ba0d875355aef5d52224
X <70487315+XWasHere@users.noreply.github.com>
2021-03-24 09:29:09 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const SETTINGSPATH = "./config.json"; //TODO: This should go in %APPDATA%/Roaming when finished, A way to configure this would also be pretty neat, but I'm not sure how that would work.
+
+function login() {
+  
+}
+
+function getConfig() {
+  
+}
+
+async function main() {
+  let config = getConfig(); // get user config
+  let running= 1;
+  while (running) {
+    break;
+  }
+}
+
+main();
