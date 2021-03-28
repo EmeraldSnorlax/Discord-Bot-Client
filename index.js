fda6782c7f4f7db25c5e7aa102fe6c7bb80a8fab
X-San <70487315+XWasHere@users.noreply.github.com>
2021-03-28 14:09:26 -0400

    crap

--- index.js
+++ index.js
@@ -1,216 +1,212 @@
-process.stdin.setRawMode(true);
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-
 const fs = require('fs');
-const tlib = require('./tlib');
-const enums = require('./enum');
-const discord = require('./discord');
-const SETTINGSPATH = './config.json'; //TODO: This should go somewhere in ~ when finished, A way to configure this would also be pretty neat, but I'm not sure how that would work
-const limit = 2000; // shitcords character limit.
+const EventEmitter = require('events');
+const WebSocket = require('ws');
 
-let termsize, client;
+const INTENT = {
+	GUILDS: 1 << 0,
+	GUILD_MEMBERS: 1 << 1,
+	GUILD_BANS: 1 << 2,
+	GUILD_EMOJIS: 1 << 3,
+	GUILD_INTEGRATIONS: 1 << 4,
+	GUILD_WEBHOOKS: 1<<5,
+	GUILD_INVITES: 1 << 6,
+	GUILD_VOICE_STATES: 1 << 7,
+	GUILD_PRESENCES: 1 << 8,
+	GUILD_MESSAGES: 1 << 9,
+	GUILD_MESSAGE_REACTIONS: 1 << 10,
+	GUILD_MESSAGE_TYPING: 1 << 11,
+	DIRECT_MESSAGES: 1 << 12,
+	DIRECT_MESSAGE_REACTIONS: 1 << 13,
+	DIRECT_MESSAGE_TYPING: 1 << 14
+};
 
-function fill(x1, y1, x2, y2, i) {
-	let c = i.char || ' ';
-	tlib.setCursorPos(x1, y1);
-	for (let i = x1; i <= x2; i++) {
-		for (let ii = y1; ii <= y2; ii++) {
-			process.stdout.write(c);
-		}
-		tlib.setCursorPos(i, y1);
-	}
+const OPCODE = {
+	EVENT: 0,
+	HEARTBEAT: 1,
+	IDENTIFY: 2,
+	RESUME: 6,
+	HELLO: 10
+};
+
+const EVENT = {
+	READY: 'READY'
 }
 
-// User input component. Needed to prevent user input from being mixed with app output (which will be in its own component above the input)
-class UInput {
-	sh;
-	sw;
-	x;
-	y;
-	inputbuffer;
-	len;
-	pos;
-	constructor(opts) {
-		this.sw = opts.width || 20;
-		this.sh = opts.height || 1;
-		this.x = opts.x || 1;
-		this.y = opts.y || 1;
+class GatewayConnection extends EventEmitter {
+	static GatewayURL = "wss://gateway.discord.gg/"
 
-		this.buf = opts.buf || Buffer.alloc(limit);
-
-		this.len = 0;
-		this.pos = 0;
+	constructor() {
+		super()
+		this.sequence = null;
+		this.hheartbeat = undefined;
 	}
 
-	resize(opts) {
-		this.sw = opts.width || this.sw;
-		this.sh = opts.height || this.sh;
-	}
+	connect(opts) {
+		this.socket = new WebSocket(GatewayConnection.GatewayURL);
+		this.token = opts.token;
 
-	move(opts) {
-		this.x = opts.x || this.x;
-		this.y = opts.y || this.y;
-	}
-
-	putchar(c) {
-		this.buf.copy(this.buf, this.pos + 1, this.pos, this.len);
-		this.buf.write(c, this.pos);
-		this.len++;
-		this.pos++;
-	}
-
-	handleBackspace() {
-		this.buf.write('\x00', this.pos - 1);
-		this.buf.copy(this.buf, this.pos - 1, this.pos);
-		this.pos--;
-		this.len--;
-	}
-
-	handleUp() {
-		if (this.pos - this.sw <= 0) return;
-		this.pos += this.sw;
-	}
-
-	handleDown() {
-		if (this.pos + this.sw >= this.len) return;
-		this.pos += this.sw;
-	}
-
-	handleRight() {
-		if (this.pos >= this.len) return;
-		this.pos++;
-	}
-
-	handleLeft() {
-		if (this.pos <= 0) return;
-		this.pos--;
-	}
-
-	draw() {
-		fill(this.x, this.y, this.x + this.sh - 1, this.y + this.sw - 1, {
-			char: '#'
+		this.socket.on('message', (data) => {
+			data = JSON.parse(data);
+			this.sequence = data.s || this.sequence; // update sequence for heartbeat.
+			if (data.op == OPCODE.EVENT) {
+				this.emit(data.t, data)
+			}
+			else if (data.op == OPCODE.HELLO) {// helo from gateway 
+				this.hheartbeat = setInterval(this.heartbeat.bind(this), data.d.heartbeat_interval);
+				if (!opts.session) {
+					this.identify({
+						token: this.token,
+						intents: 0 | INTENT.GUILD_MESSAGES | INTENT.GUILD_MESSAGE_REACTIONS | INTENT.GUILD_MESSAGE_TYPING | INTENT.DIRECT_MESSAGES | INTENT.DIRECT_MESSAGE_REACTIONS | INTENT.DIRECT_MESSAGE_TYPING | INTENT.GUILD_EMOJIS | INTENT.GUILD_BANS,
+					})
+				}
+				else {
+					this.resume({
+						token: this.token,
+						seq: opts.sequence,
+						sid: opts.session
+					})
+				}
+			}
+			console.debug(data);
 		});
-		process.stdout.write(this.buf.toString('utf8', 0, this.len));
+		
+		this.socket.on('error', (err) => {
+			console.debug(err);
+		});
+
+		this.socket.on('close', (code, reason) => {
+			console.debug(`CLOSE: ${reason} (${code})`)
+			this.emit('close');
+		});
+	}
+
+	heartbeat() {
+		this.socket.send(JSON.stringify({
+			op:OPCODE.HEARTBEAT, // HEARTBEAT
+			d: this.sequence
+		}))
+	}
+
+	identify(opts) {
+		this.socket.send(JSON.stringify({
+			op:OPCODE.IDENTIFY, // IDENTIFY
+			d: {
+				token: opts.token,
+				intents: opts.intents,
+				properties: opts.properties || {
+
+				}
+			}
+		}))
+	}
+
+	resume(opts) {
+		this.socket.send(JSON.stringify({
+			op:OPCODE.RESUME,
+			d: {
+				token: opts.token,
+				session_id: opts.sid,
+				seq: opts.seq
+			}
+		}))
+	}
+	// Gateway event classes.
+}
+
+class User {
+	username; discriminator;
+	
+	static parseJSON(obj) {
+		let u = new User();
+		u.username = obj.username;
+		u.discriminator = obj.discriminator;
+		return u
+	}
+
+	get tag() {
+		return `${this.username}#${this.discriminator}`
 	}
 }
 
-class MessageComponent {
-	sh;
-	sw;
-	x;
-	y;
-	buf;
-	line;
-	col;
-
-	constructor(opts) {
-		this.x = opts.x || 1;
-		this.y = opts.y || 1;
-		this.sw = opts.width;
-		this.sh = opts.height;
-		this.line = 1;
-		this.col = 1;
-		this.buf = Buffer.alloc(this.sw * this.sh);
-	}
-
-	write(data) {
-		tlib.setCursorPos(this.line, this.col);
-		String(data)
-			.split('')
-			.forEach(d => {
-				this.buf.write(d, this.line * this.sw + this.col);
-				this.col++;
-			});
-		this.render();
-	}
-
-	render() {
-		tlib.setCursorPos(this.x, this.y);
-		process.stdout.write(this.buf);
+class Message {
+	static parseJSON(obj) {
+		let m = new Message();
+		m.IsDM = (obj.guild_id == undefined)
+		m.content = obj.content;
+		m.author = User.parseJSON(obj.author);
+		m.channel = obj.channel_id;
+		return m;
 	}
 }
 
-function login() {}
+class Client extends EventEmitter {
+	constructor() {
+		super();
+	}
 
-function getConfig() {
-	if (fs.existsSync(SETTINGSPATH)) {
-		return JSON.parse(fs.readFileSync(SETTINGSPATH));
-	} else return {};
+	GWConnect(opts) {
+		return new Promise((res,rej) => {
+			function loginCallback(data) {
+				console.debug(data)
+				res(data.d.session_id);
+			}
+
+			this.gateway = new GatewayConnection()
+			this.gateway.on('close', ()=>{console.log("connection closed")});
+			this.gateway.once('READY', loginCallback);
+			this.gateway.on('MESSAGE_CREATE', (m) => {
+				this.emit('message',Message.parseJSON(m.d));
+			})
+			this.gateway.connect(opts);
+		});
+	}
+
+	async login(opts) {
+		if (opts.token) {
+			this.token = opts.token;
+			console.log (opts)
+			if (opts.session) {
+				this.session = await this.GWConnect({token: this.token, sequence: opts.sequence, session:opts.session})
+			}
+			else this.session = await this.GWConnect({token:this.token});
+			return this.session;
+		}
+	}
 }
-
-function handleCommand(cmd) {}
 
 async function main() {
-	client = new discord.Client();
-	let config = getConfig(); // get user config
-	let running = 1;
+	// Config file
+	let config;
+	if (fs.existsSync("./config.json")) {
+		config = JSON.parse(fs.readFileSync('./config.json'))
+	}
+	else {
+		config = {}
+	}
+	console.debug(config)
 
-	if (!config['termsize']) {
-		termsize = await tlib.getSize();
-	} else {
-		termsize = config.termsize;
+	let client = new Client();
+	// Login
+	if (fs.existsSync('./session')&&false) {
+		let sd = JSON.parse(fs.readFileSync('./session'));
+		console.log(sd)
+		sid = await client.login({token:config.token, session: sd.sid, sequence: sd.seq})
+		console.log(sid)
+		fs.writeFileSync('./session', JSON.stringify({sid: sid, seq:0}));
+	}
+	if (config.token && !client.session) {
+		sid = await client.login({token:config.token});
+		fs.writeFileSync('./session', JSON.stringify({sid: sid, seq: 0}));
 	}
 
-	let output = new MessageComponent({
-		height: termsize.height - 1,
-		width: termsize.width,
-		x: 1,
-		y: 1
-	});
+	setInterval(() => {
+		fs.writeFileSync('./session', JSON.stringify({sid: sid, seq: client.gateway.sequence}))
+	}, 1000);
 
-	console.log = function(data) {
-		output.write(data);
-	};
-
-	let uinput = new UInput({
-		height: 1,
-		width: termsize.width,
-		x: termsize.height,
-		y: 1
-	});
-	uinput.draw();
-
-	console.log('Discord-CLI 0.0.1\n');
-
-	if (config['auth']) {
-		console.log('Logging in\n');
-		let auth;
-		if (config.auth.location == enums.AUTHLOCATION.ENV) {
-			auth = process.env.auth;
-		}
-
-		if (config.auth.type == enums.AUTH.BOT) {
-		}
-	}
-
-	process.stdin.on('data', c => {
-		//console.log([c,tlib.CSI])
-		//console.log(c==`${tlib.CSI}A`)
-		if (c.charCodeAt(0) == 127) {
-			uinput.handleBackspace();
-		} else if (c == `${tlib.CSI}A`) {
-			uinput.handleUp();
-		} else if (c == `${tlib.CSI}B`) {
-			uinput.handleDown();
-		} else if (c == `${tlib.CSI}C`) {
-			uinput.handleRight();
-		} else if (c == `${tlib.CSI}D`) {
-			uinput.handleLeft();
-		} else if (c == `\r`) {
-			if ((uinput.buf[0] = '/')) {
-				handleCommand(uinput.buf.toString('utf8', 1, uinput.len));
-			}
-			console.log(uinput.buf.toString('utf8', 0, uinput.len));
-			uinput.pos = 0;
-			uinput.len = 0;
-			uinput.buf.fill('\x00');
-		} else {
-			uinput.putchar(c);
-		}
-		uinput.draw();
-	});
-	await client.login();
+	client.on('message', (m) => {
+		console.log(`${m.IsDM ? '[#DM] ':`[#${m.channel}]`}<${m.author.tag}> ${m.content}`);
+	})
 }
-
+console.debug = () => {}
+console.log(process.stdout.setEn)
 main();
